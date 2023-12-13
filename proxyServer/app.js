461056e1f9587d38bf36bc81f83406a3c030b289
Nitish Manral <nitishmanral9@gmail.com>
2023-12-13 15:10:37 +0530

    all client side api's resolved

--- proxyServer/app.js
+++ proxyServer/app.js
@@ -10,7 +10,8 @@ app.use('/api', createProxyMiddleware({
     changeOrigin: true,
     pathRewrite: {
         [`^/api`]: 'sunbase/portal/api', // rewrite path
-    }
+    },
+    timeout: 5000,
 }));
 app.listen(3000, () => {
     console.log('Proxy server is running on port 3000');
