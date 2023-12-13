d9cfd4025ed07c70b5c2b1ae85bbb2079d14c049
Nitish Manral <nitishmanral9@gmail.com>
2023-12-13 10:39:25 +0530

    Initial Commit

---
+++ proxyServer/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+const cors = require('cors');
+
+const app = express();
+app.use(cors()); // Use cors middleware here
+
+app.use('/api', createProxyMiddleware({ 
+    target: 'https://qa2.sunbasedata.com', // target API server here
+    changeOrigin: true,
+    pathRewrite: {
+        [`^/api`]: '/sunbase/portal/api', // rewrite path
+    },
+}));
+//https://qa2.sunbasedata.com/sunbase/portal/api/assignment.jsp
+app.listen(3000, () => {
+    console.log('Proxy server is running on port 3000');
+});
